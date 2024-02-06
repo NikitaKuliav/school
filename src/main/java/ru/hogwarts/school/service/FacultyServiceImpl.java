615b89fd5660ac854e672d70761696356b00b602
Nikita <athlon64x2600@gmail.com>
2024-02-06 23:09:41 +0200

    4.4 Логирование и конфигурирование

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -11,6 +13,8 @@ import java.util.List;
 @Service
 public class FacultyServiceImpl implements FacultyService{
 
+    private final Logger logger = LoggerFactory.getLogger(FacultyService.class);
+
     private final FacultyRepository facultyRepository;
 
     public FacultyServiceImpl(FacultyRepository facultyRepository) {
@@ -20,40 +24,48 @@ public class FacultyServiceImpl implements FacultyService{
 
     @Override
     public Faculty getFaculty(Long id) {
+        logger.info("Was invoked method getFaculty");
         return facultyRepository.findById(id).get();
     }
 
     @Override
     public Faculty addFaculty(Faculty faculty) {
+        logger.info("Was invoked method addFaculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public void deleteFaculty(Long id) {
+        logger.info("Was invoked method deleteFaculty");
         facultyRepository.deleteById(id);
 
     }
 
     @Override
     public Faculty updateFaculty(Long id, Faculty faculty) {
+        logger.info("Was invoked method updateFaculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public List<Faculty> getFacultiesByColor(String color) {
+        logger.info("Was invoked method getFacultiesByColor");
         return facultyRepository.findByColor(color);
     }
 
     public List<Faculty> getFacultiesByNameIgnoreCase(String name) {
+        logger.info("Was invoked method getFacultiesByNameIgnoreCase");
         return facultyRepository.findByNameIgnoreCase(name);
     }
 
     public List<Faculty> getFacultiesByColorIgnoreCase(String color) {
+        logger.info("Was invoked method getFacultiesByColorIgnoreCase");
         return facultyRepository.findByColorIgnoreCase(color);
     }
 
     @Override
     public Collection<Student> getFacultyStudents(Long facultyId) {
+        logger.info("Was invoked method getFacultyStudents");
         return facultyRepository.findStudentsById(facultyId);
     }
 }
